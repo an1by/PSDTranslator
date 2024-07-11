b622aa78cc6a11f1fca768431a8d3639b444506c
an1by <mrkillermafia@gmail.com>
2024-07-11 16:16:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PSD Translator
+## What is that?
+This script will translate all your `.psd` file.\
+All file `=` all layers' names and itself file name.\
+
+## How to use
+1. Download Python (3.10+);
+2. Run `pip install deep-translator psd-tools`;
+3. Download this repository or `main.py` manually;
+4. Run `main.py` via Python3;
+5. Follow the instructions.
+
+## Little chronicle
+This script I created to translate the `.psd` file from Chinese/Japanese users for future rigging convenience in `Live2D Cubism`.
---
+++ main.py
@@ -0,0 +1,30 @@
+from deep_translator import GoogleTranslator
+import psd_tools
+
+inputName = input("PSD-file name (example: Model.psd): ")
+outputName = input("Output PSD-file name (example: output.psd. Default - translated PSD-file name): ")
+targetLanguage = input("Target Language (Default - en): ")
+if targetLanguage.strip() == '':
+    targetLanguage = 'en'
+
+translator = GoogleTranslator(source='auto', target=targetLanguage)
+if outputName.strip() == '':
+    outputName = translator.translate(inputName)
+
+psd = psd_tools.PSDImage.open(inputName)
+
+def renameLayer(group: psd_tools.api.layers.Group):
+    previous = group.name
+    translated = translator.translate(previous)
+    if translated is None:
+        translated = previous
+    group.name = translated
+    print(f"Translated: {previous} -> {translated}")
+
+
+c = 0
+for layer in psd.descendants():
+    c += 1
+    renameLayer(layer)
+
+psd.save(outputName)
